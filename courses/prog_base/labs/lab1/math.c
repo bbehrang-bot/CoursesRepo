71fa83e452801f67f7af8f8da2c9af6b55dcfa22
bbehrang <bbehrang95@gmail.com>
2015-09-30 11:06:38 +0300

    v1

---
+++ courses/prog_base/labs/lab1/math.c
@@ -0,0 +1,23 @@
+#import <math.h>
+double calc(double x, double y, double z) {
+   double a;
+  double n0 =5;
+  double n1 = 2;
+  double a0,a1,a2;
+
+if(x-y<0||x+y==0||sin(y)==0||x-y==0||z==0)
+{
+    a = NAN;
+}
+else{
+  a0 = pow(x,y+1)/pow(x-y,1/z);
+
+  a1 = y/((n0+1)*abs(x+y));
+  a2 = pow(x+n1,1/abs(sin(y)));
+
+     a=a0+a1+a2;
+ }
+
+
+   return a;
+}
