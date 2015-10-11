be009ba33b2e48d519b78c42db905f0c912f3e39
bbehrang <bbehrang95@gmail.com>
2015-10-11 21:22:46 +0300

    v1

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -4,14 +4,22 @@
 
 
 double long2double(long long x){
-int i = 0;
+int i = 1;
     double b[64]={0};
     double sign=0;
     double exp=0;
     double fr=0;
     double answer;
-
+     double nLong = x;
     i=0;
+if(x<0){
+    x=fabs(x);
+    x=x-1;
+    b[0]=1;
+
+}
+else
+    b[0]=0;
 
     while(x!=0){
 
@@ -19,17 +27,31 @@ int i = 0;
         x=x/2;
         i++;
     }
+     if(nLong<0){
+
+        for(i=1;i<64;i++){
+            if(b[i]==0){
+                b[i]=1;
+
+            }
+            else if(b[i]==1){
+                b[i]=0;
+            }
+        }
+    }
     double c[64] = {0};
-    for(i=0;i<64;i++){
+    for(i=1;i<64;i++){
         c[63-i]=b[i];
 
     }
+
 
 
     if(c[0]==1)
         sign = -1;
     else
         sign= 1;
+
 
      for(i=1;i<=11;i++){
 
@@ -54,9 +76,9 @@ int i = 0;
         answer = -INFINITY;
      else if(exp>0&&exp<2047)
          answer = sign*(pow(2,exp-1023))*(1+fr);
-     else if(exp==0&&fr!=0)
+     else if(exp=0&&fr!=0)
         answer = sign*pow(2,-1022)*fr;
-     else if(exp==0&&fr==0&&sign==-1)
+     else if(exp=0&&fr==0&&sign==-1)
         answer = -0;
      else if(exp==0&&fr==0&&sign==1)
         answer =+0;
@@ -68,7 +90,7 @@ int i = 0;
 int main()
 {
 
-  double p = long2double(4620000000000000000);
+  double p = long2double(-4620000000000000000);
 
 
 
