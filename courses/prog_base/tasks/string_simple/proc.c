a0de132f78140b3d487a595cbe5c3d3caa0a1fc9
bbehrang <bbehrang95@gmail.com>
2015-11-01 23:59:41 +0200

    strcpy(resultStr,"");

--- courses/prog_base/tasks/string_simple/proc.c
+++ courses/prog_base/tasks/string_simple/proc.c
@@ -1,41 +1,42 @@
- char * process(char * resultStr, const char * textLines[], int linesNum, const char * extraStr){
+char * process(char * resultStr, const char * textLines[], int linesNum, const char * extraStr){
 
 
 
-    int i=0;
-    char h[100]={0};
-    char ans[100];
-    int j = 0;
-    int counter=0;
-    int howmany=0;
+	int i = 0;
+	char h[100] = { 0 };
+	char ans[100];
+	int j = 0;
+	int counter = 0;
+	int howmany = 0;
+	strcpy(resultStr, "");
 
 
-    for(i;i<linesNum;i++)
-    {
-        counter =0;
-        for(j=0;j<strlen(textLines[i]);j++)
-        {
-            if (textLines[i][j] != ' '&&textLines[i][j] != '\0')
+	for (i; i<linesNum; i++)
+	{
+		counter = 0;
+		for (j = 0; j<strlen(textLines[i]); j++)
+		{
+			if (textLines[i][j] != ' '&&textLines[i][j] != '\0')
 				counter++;
 
-        }
-        if(counter<10)
-        {
-            howmany++;
-            strcat(resultStr,textLines[i]);
-            strcat(resultStr,",");
-        }
-    }
+		}
+		if (counter<10)
+		{
+			howmany++;
+			strcat(resultStr, textLines[i]);
+			strcat(resultStr, ",");
+		}
+	}
 
-    sprintf(h,"%i",howmany);
-    strcpy(ans,h);
-    strcat(ans," ");
-    strcat(ans,resultStr);
-    strcpy(resultStr,ans);
-
-
+	sprintf(h, "%i", howmany);
+	strcpy(ans, h);
+	strcat(ans, " ");
+	strcat(ans, resultStr);
+	strcpy(resultStr, ans);
 
 
 
-return resultStr;
+
+
+	return resultStr;
 }
