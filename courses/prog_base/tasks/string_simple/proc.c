b6d8d808f91cf84b30432bc86f87ecd3a90f2570
bbehrang <bbehrang95@gmail.com>
2015-11-01 19:36:26 +0200

    strcat saved me

--- courses/prog_base/tasks/string_simple/proc.c
+++ courses/prog_base/tasks/string_simple/proc.c
@@ -1,56 +1,30 @@
-const char * process(char * resultStr, const char * textLines[], int linesNum, const char * extraStr)
-{
-
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int lineNumIndex = 0;
-	int counter = 0;
-	int lineLength = 0;
-
-	int z = 0;
-	int x = 0;
-
-
-	for (i; i<linesNum; i++)
-	{
-
-		counter = 0;
-		lineLength = strlen(textLines[i]);
-
-
-		for (j = 0; j < lineLength; j++)
-		{
-			if (textLines[i][j] != ' '&&textLines[i][j] != '\0')
+const char * process(char * resultStr, const char * textLines[], int linesNum, const char * extraStr){
+    int size= strlen(textLines);
+    char *index;
+    int i=0;
+    int j = 0;
+    int counter=0;
+    const char blank = ' ';
+    for(i;i<linesNum;i++)
+    {
+        counter =0;
+        for(j=0;j<strlen(textLines[i]);j++)
+        {
+            if (textLines[i][j] != ' '&&textLines[i][j] != '\0')
 				counter++;
 
-		}
-
-		if (counter<10)
-		{
-
-
-
-			resultStr[k] = i + '0';
-			k++;
-			resultStr[k] = ' ';
-			k++;
-				for (x = 0; x < lineLength; x++)
-				{
-					resultStr[k] = textLines[i][x];
-					k++;
-				}
-				resultStr[k] = ',';
-				k++;
-
-
-		}
-	}
-
-	return resultStr;
+        }
+        if(counter<10)
+        {
+            sprintf(index,"%i",i+1); /*if its number of line i+1 , if its the index i */
+            strcat(resultStr,index);
+            strcat(resultStr," ");
+            strcat(resultStr,textLines[i]);
+            strcat(resultStr,",");
+        }
+    }
 
 
 
-
-
+return resultStr;
 }
