7c35e69e16b7751a9a2935320babf13e7deec810
bbehrang <bbehrang95@gmail.com>
2015-11-29 21:01:23 +0200

    2

--- courses/prog_base/tasks/files/files.c
+++ courses/prog_base/tasks/files/files.c
@@ -1,3 +1,4 @@
+#include <string.h>
 void fprocess(const char * pread, const char * pwrite)
 {
     FILE * fpr;
@@ -25,7 +26,7 @@ void fprocess(const char * pread, const char * pwrite)
                 a[0]=fgetc(fpr);
 
             }
-            tmp=malloc(lineLength);
+            tmp=malloc(lineLength*sizeof(char));
             strcpy(tmp,"");
             fseek(fpr,-lineLength,SEEK_END);
             fgets(tmp,lineLength+1,fpr);
@@ -43,7 +44,7 @@ void fprocess(const char * pread, const char * pwrite)
 
     int i;
     char *tmpResult;
-    tmpResult = malloc(lineLength*2);
+    tmpResult = malloc(lineLength*sizeof(char));
     strcpy(tmpResult,"");
     for(i=0;i<strlen(tmp);i++)
     {
@@ -54,10 +55,13 @@ void fprocess(const char * pread, const char * pwrite)
     fclose(fpr);
     FILE *result;
     result = fopen(pwrite,"w");
+
+
     fprintf(result,tmpResult);
-    free(tmp);
-    free(tmpResult);
-    fclose(result);
+
+
+	 fclose(result);
+
 
 
 }
