2546333c095df9a3e9baf1c10a7645535244d1ea
bbehrang <bbehrang95@gmail.com>
2015-12-14 15:54:58 +0200

    v2
    Sorry I was checking only the result of exam and totally forgot about
    this task

--- courses/prog_base/tasks/recursion/main.c
+++ courses/prog_base/tasks/recursion/main.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char lastChar(char *myStr,int i)
+//Sorry I was checking only the result of exam and totally forgot about this task
+char lastChar(char *myStr,int i,char result)
 {
+
     char l=myStr[i];
     i++;
     if(i<=strlen(myStr))
     {
-    if(myStr[i]=='\0')
-        {
-            if(l>='a'&&l<='z')
-                return l;
-            else
-                return '0';        }
-    else
-        lastChar(myStr,i);
+    if(l>='a'&&l<='z')
+    {
+        result = l;
     }
+    lastChar(myStr,i,result);
+    }
+    else
+        return result;
+
 }
 int main()
 {
     char v[100];
+    char result;
     gets(v);
-    char q =lastChar(v,0);
+    char q =lastChar(v,0,'0');
     printf("%c",q);
     return 0;
 }
